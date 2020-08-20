cd83b5f098f717591efc9041387d1e257273fefa
leekyungho112 <epsoe@naver.com>
2020-08-20 14:10:16 +0900

    done

--- src/Movie.js
+++ src/Movie.js
@@ -6,15 +6,21 @@ function Movie({
     year,
     title,
     summary,
-    poster})
+    poster,
+    genres
+    })
     {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div class="movie_data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie_data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">{genres && genres.map((genre,index) => (
+                    <li key={index} className="genres__genre" >{genre}</li>
+                )
+                    )}</ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
@@ -25,7 +31,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 
 }
 export default Movie;
